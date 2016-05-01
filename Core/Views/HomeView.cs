0ceac843e17bb9606a631db5871ec579716191f5
Ione Souza Junior <junior@ionixjunior.com.br>
2016-05-01 19:26:05 -0300

    Adicionado projeto Windows Phone

---
+++ Core/Views/HomeView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Core.Views
+{
+	public class HomeView : ContentPage
+	{
+		public HomeView ()
+		{
+			Title = "Home";
+			Content = new StackLayout { 
+				Children = {
+					new Label { Text = "Hello ContentPage" }
+				}
+			};
+		}
+	}
+}
+
+
