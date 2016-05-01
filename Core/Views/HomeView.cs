e192210485e18adfb94b81e97b9cd1fba36a2ff8
Ione Souza Junior <junior@ionixjunior.com.br>
2016-05-01 20:04:01 -0300

    Adicionado exemplo do SwitchCell

--- Core/Views/HomeView.cs
+++ Core/Views/HomeView.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 
 using Xamarin.Forms;
+using Core.Views.Switch;
 
 namespace Core.Views
 {
@@ -9,11 +10,35 @@ namespace Core.Views
 		public HomeView ()
 		{
 			Title = "Home";
-			Content = new StackLayout { 
-				Children = {
-					new Label { Text = "Hello ContentPage" }
+
+			Content = new TableView() {
+				Intent = TableIntent.Menu, 
+				Root = new TableRoot() {
+					new TableSection() {
+						GetTextCell("Desabilitando SwitchCell", typeof(SwitchCellView), Open)
+					}
 				}
 			};
+		}
+
+		private TextCell GetTextCell(string text, Type page, EventHandler eventHandler)
+		{
+			TextCell textCell = new TextCell () {
+				BindingContext = page, 
+				Text = text
+			};
+			textCell.Tapped += eventHandler;
+
+			return textCell;
+		}
+
+		private void Open(object sender, EventArgs args)
+		{
+			TextCell textCell = (TextCell)sender;
+			Type pageType = (Type)textCell.BindingContext;
+
+			Page page = (Page)Activator.CreateInstance (pageType);
+			Navigation.PushAsync (page);
 		}
 	}
 }
---
+++ Core/Views/Switch/SwitchCellView.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Core.Views.Switch
+{
+	public class SwitchCellView : ContentPage
+	{
+		public SwitchCellView ()
+		{
+			Title = "SwitchCell";
+
+			Content = new TableView() {
+				Root = new TableRoot() {
+					new TableSection() {
+						GetSwitchCell("Habilitado", true), 
+						GetSwitchCell("Desabilitado", false)
+					}
+				}
+			};
+		}
+
+		private SwitchCell GetSwitchCell(string text, bool isEnabled)
+		{
+			return new SwitchCell () {
+				Text = text, 
+				IsEnabled = isEnabled, 
+				On = true
+			};
+		}
+	}
+}
+
+
