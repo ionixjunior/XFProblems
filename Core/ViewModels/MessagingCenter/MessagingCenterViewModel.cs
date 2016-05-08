82e4aadf29085889a2775f3f321db0d85dbadf41
Ione Souza Junior <junior@ionixjunior.com.br>
2016-05-08 00:55:19 -0300

    Aprimorado exemplo

---
+++ Core/ViewModels/MessagingCenter/MessagingCenterViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Core.Models;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Core.ViewModels.MessagingCenter
+{
+	public class MessagingCenterViewModel : BaseViewModel
+	{
+		private ClickModel _click { get; set; }
+
+		public ClickModel Click
+		{
+			get { return _click; }
+			set 
+			{
+				_click = value;
+				INotifyPropertyChanged ();
+			}
+		}
+
+		public ICommand ClickCommand { get; private set; }
+
+		public MessagingCenterViewModel()
+		{
+			Click = new ClickModel () {
+				Total = 0
+			};
+
+			ClickCommand = new Command (() => ClickExec());
+		}
+
+		public void ClickExec()
+		{
+			Click.Total++;
+			Xamarin.Forms.MessagingCenter.Send<ClickModel>(Click, "UpdateClicks");
+		}
+	}
+}
+
--- Core/Views/MessagingCenter/MessagingCenterView.cs
+++ Core/Views/MessagingCenter/MessagingCenterView.cs
@@ -2,61 +2,56 @@
 
 using Xamarin.Forms;
 using Core.Models;
+using Core.ViewModels.MessagingCenter;
 
 namespace Core.Views.MessagingCenter
 {
 	public class MessagingCenterView : ContentPage
 	{
-		private ClickModel _click { get; set; }
-
-		public ClickModel Click
-		{
-			get 
-			{
-				if (_click == null) 
-				{
-					_click = new ClickModel () {
-						Total = 0
-					};
-				}
-
-				return _click;
-			}
-			set 
-			{
-				_click = value;
-			}
-		}
-
 		private string _message { get; } = "Clicks: {0}";
+
+		private MessagingCenterViewModel _viewModel { get; set; }
+
+		private Label _label { get; set; }
 
 		public MessagingCenterView ()
 		{
 			Title = "Messaging Center";
+			_viewModel = new MessagingCenterViewModel ();
 
+			BuildToolbarItems ();
+			BuildContent ();
+			SubscribeMessage ();
+		}
+
+		private void BuildToolbarItems()
+		{
 			ToolbarItems.Add (new ToolbarItem() {
 				Text = "Update click", 
-				Command = new Command(() => { 
-					Click.Total++;
-					Xamarin.Forms.MessagingCenter.Send<ClickModel>(Click, "UpdateClicks");
-				})
+				Command = _viewModel.ClickCommand
 			});
+		}
 
-			Label label = new Label () {
+		private void BuildContent()
+		{
+			_label = new Label () {
 				HorizontalOptions = LayoutOptions.CenterAndExpand, 
 				VerticalOptions = LayoutOptions.CenterAndExpand, 
-				Text = string.Format(_message, Click.Total)
+				Text = string.Format(_message, _viewModel.Click.Total)
 			};
 
 			Content = new StackLayout { 
 				Children = {
-					label
+					_label
 				}
 			};
+		}
 
+		private void SubscribeMessage()
+		{
 			Xamarin.Forms.MessagingCenter.Subscribe<ClickModel> (this, "UpdateClicks", (sender) => {
 				System.Diagnostics.Debug.WriteLine(string.Format("UpdateClicks {0}...", sender.Total));
-				label.Text = string.Format(_message, sender.Total);
+				_label.Text = string.Format(_message, sender.Total);
 			});
 		}
 	}
